25550a4359a452149d679a30bc7dad327b45b918
zhouchao941105 <zhouchao06@qq.com>
2018-01-07 21:41:12 +0800

    引入socket.io

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 ## A react demo bundled with webpack
 **用于学习webpack**
+已实现热更新
---
+++ server/server.js
@@ -0,0 +1,12 @@
+var server = require('http').createServer();
+var io=require('socket.io')(server);
+io.on('connection',function(client){
+    client.on('event',function(msg){
+        console.log(msg);
+    })
+    client.on('disconnect',function () {
+        
+    })
+    console.log(client);
+})
+server.listen(12345);
